aafedff536f84edb8d5c18a81da8d46a93a6080c
Tristan White <oliver.tristan@gmail.com>
2023-07-27 12:56:25 +0200

    add npm link

--- _inc/footer.php
+++ _inc/footer.php
@@ -6,7 +6,8 @@
 			<p><small>The library is inspired by the <a href="https://dev.to/wgao19/night-mode-with-mix-blend-mode-difference-23lm">work of Wei Gao</a></small></p>
 			<a href="#docs">Documentation</a> |
 			<a href="#changelog">Changelog</a> |
-			<a href="https://github.com/triss90/Darky" target="_blank">GitHub</a> <br>
+			<a href="https://github.com/triss90/Darky" target="_blank">GitHub</a> |
+			<a href="https://www.npmjs.com/package/darkyjs" target="_blank">npm</a> <br>
 			<a href="/lib/1.0.0/darky.min.js" download>Download</a> |
 			<a href="https://github.com/triss90/Darky/issues" target="_blank">Request feature</a> |
 			<a href="https://github.com/triss90/Darky/issues" target="_blank">Report bug</a>
