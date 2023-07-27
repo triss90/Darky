09c90d76fb354b4600a1e3d080420a7b5770868f
Tristan White <oliver.tristan@gmail.com>
2023-07-27 09:49:51 +0200

    Fix github link

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 		<a href="/">Darky.js</a>
 		<ul>
 			<li><a href="#docs">Docs</a></li>
-			<li><a href="https: //github.com/triss90/Darky" target="_blank">GitHub</a></li>
+			<li><a href="https://github.com/triss90/Darky" target="_blank">GitHub</a></li>
 		</ul>
 	</nav>
 
