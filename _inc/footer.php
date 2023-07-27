d2d46491b356385800c4efc3be93ed3729ee0de0
Tristan White <oliver.tristan@gmail.com>
2023-07-27 14:26:43 +0200

    Add copyright

--- _inc/footer.php
+++ _inc/footer.php
@@ -2,7 +2,7 @@
 <footer>
 	<section>
 		<div style="text-align:center">
-			<p>Made by <a href="https://triss.dev">Tristan White</a></p>
+			<p>Made by <a href="https://triss.dev">Tristan White</a> &copy; 2023-<?php echo date("Y"); ?></p>
 			<p><small>The library is inspired by the <a href="https://dev.to/wgao19/night-mode-with-mix-blend-mode-difference-23lm">work of Wei Gao</a></small></p>
 			<a href="#docs">Documentation</a> |
 			<a href="#changelog">Changelog</a> |
