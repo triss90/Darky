76183509165e4af684607a2cd0a8bdb70d415e09
Tristan White <oliver.tristan@gmail.com>
2023-07-29 16:13:05 +0200

    Update guide cta

--- _inc/footer.php
+++ _inc/footer.php
@@ -18,7 +18,10 @@
 	</section>
 </footer>
 
-<span class="guide-text">This is Darky.js</span>
+<span class="guide-text">
+	This is Darky.js<br>
+	Click me!
+</span>
 <svg class="guide-arrow" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 43.1 85.9" xml:space="preserve">
   <path stroke-linecap="round" stroke-linejoin="round" class="draw-arrow" d="M11.3,2.5c-5.8,5-8.7,12.7-9,20.3s2,15.1,5.3,22c6.7,14,18,25.8,31.7,33.1" />
   <path stroke-linecap="round" stroke-linejoin="round" class="draw-arrow tail-1" d="M40.6,78.1C39,71.3,37.2,64.6,35.2,58" />
