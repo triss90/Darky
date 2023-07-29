613e20ddc97c7504c7237608e6e2846a511acb4b
Tristan White <oliver.tristan@gmail.com>
2023-07-29 11:45:55 +0200

    Update external links

--- _inc/footer.php
+++ _inc/footer.php
@@ -4,13 +4,16 @@
 		<div style="text-align:center">
 			<p>Made by <a href="https://triss.dev">Tristan White</a> &copy; 2023-<?php echo date("Y"); ?></p>
 			<p><small>The library is inspired by the <a href="https://dev.to/wgao19/night-mode-with-mix-blend-mode-difference-23lm">work of Wei Gao</a></small></p>
-			<a href="#docs">Documentation</a> |
-			<a href="#changelog">Changelog</a> |
-			<a href="https://github.com/triss90/Darky" target="_blank">GitHub</a> |
-			<a href="https://www.npmjs.com/package/darkyjs" target="_blank">npm</a> <br>
-			<a href="/lib/1.1.2/darky.min.js" download>Download</a> |
-			<a href="https://github.com/triss90/Darky/issues" target="_blank">Request feature</a> |
-			<a href="https://github.com/triss90/Darky/issues" target="_blank">Report bug</a>
+			<a href="#docs">Documentation</a> ·
+			<a href="#changelog">Changelog</a> ·
+			<a href="https://github.com/triss90/Darky" target="_blank">GitHub</a> ·
+			<a href="https://www.npmjs.com/package/darkyjs" target="_blank">npm</a>
+
+			<br>
+
+			<a href="/lib/1.1.2/darky.min.js" download>Download</a> ·
+			<a href="https://github.com/triss90/Darky/issues/new?assignees=&labels=&projects=&template=feature_request.md&title=I want a new feature!" target="_blank">Request feature</a> ·
+			<a href="https://github.com/triss90/Darky/issues/new?assignees=&labels=&projects=&template=bug_report.md&title=I found a bug!" target="_blank">Report bug</a>
 		</div>
 	</section>
 </footer>
